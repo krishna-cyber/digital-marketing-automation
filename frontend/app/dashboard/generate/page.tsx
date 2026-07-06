84ffa3eba1e87cf6a11f4fb7f48722a95e3a5e1a
krishna-cyber <tiwarikrishna54321@gmail.com>
2026-07-06 14:00:10 +0545

    feat: otp page added

--- frontend/app/dashboard/generate/page.tsx
+++ frontend/app/dashboard/generate/page.tsx
@@ -1,8 +1,14 @@
+import { AiPromptTextArea } from "@/components/examples/ai-command-textarea"
 import React from "react"
 import { TextGenerationDemo } from "./simple-text-generate"
 
 const page = () => {
-  return <TextGenerationDemo />
+  return (
+    <>
+      <TextGenerationDemo />
+      <AiPromptTextArea />
+    </>
+  )
 }
 
 export default page
